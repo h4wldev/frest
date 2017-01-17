f6c1c6f5930232a10fb142a11c7d4e6889c49a33
h4wldev <h4wldev@gmail.com>
2017-01-17 20:29:34 +0900

    Add order, parameter Add return item(paging) GET /api/v@/users

--- app/routes/api/v1/users/users.py
+++ app/routes/api/v1/users/users.py
@@ -5,6 +5,7 @@ from flask_restful import Resource
 from werkzeug.security import generate_password_hash
 
 from app import db, token_auth
+from app.config import DEFAULT_URL
 from app.modules import frest
 from app.modules.token import token_is_auth, token_load
 from app.modules.form_validation import Validation
@@ -20,19 +21,30 @@ class Users(Resource):
     def get(self):
         page = request.args.get('page', 0, type=int)
         limit = request.args.get('limit', 10, type=int)
+        order = request.args.get('order', 'desc')
 
         if token_is_auth(request.headers['Authorization']):
-            users = []
+            _return = {
+                'paging': {
+                    'previous': '%s%s?page=%d&limit=%d&order=%s' % (
+                        DEFAULT_URL, request.path, page if page < 1 else page - 1, limit, order
+                    ),
+                    'next': '%s%s?page=%d&limit=%d&order=%s' % (
+                        DEFAULT_URL, request.path, page + 1, limit, order
+                    )
+                },
+                'data': []
+            }
 
             users_query = UserModel.query \
-                .order_by(UserModel.id.desc()) \
+                .order_by(UserModel.id.asc() if order == 'asc' else UserModel.id.desc()) \
                 .limit(limit) \
                 .offset(page * limit)
 
             for user in users_query:
-                users.append(serialize_user(user))
+                _return['data'].append(serialize_user(user))
 
-            return users, status.HTTP_200_OK
+            return _return, status.HTTP_200_OK
 
         return 'UNAUTHORIZED', status.HTTP_401_UNAUTHORIZED
 
