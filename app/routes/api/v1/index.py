7767cfd79a5bfb6602da9fff81704107f47e87a5
h4wldev <h4wldev@gmail.com>
2017-02-02 16:20:11 +0900

    Return environment, versions

--- app/routes/api/v1/index.py
+++ app/routes/api/v1/index.py
@@ -2,6 +2,7 @@
 from flask_api import status
 from flask_restful import Resource
 
+from app.config import ENVIRONMENT, API_VERSION, APP_VERSION
 from app.modules import frest
 
 
@@ -11,4 +12,12 @@ _URL = '/'
 class Index(Resource):
     @frest.API
     def get(self):
-        return None, status.HTTP_200_OK
+        _return = {
+            'environment': ENVIRONMENT,
+            'version': {
+                'api': API_VERSION,
+                'app': APP_VERSION
+            }
+        }
+
+        return _return, status.HTTP_200_OK
