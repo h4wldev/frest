2a514110283b33e0de53f6285cec1354f80e26fa
h4wldev <h4wldev@gmail.com>
2017-02-03 09:23:20 +0900

    Logging user login history

---
+++ app/models/user_login_history_model.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+
+from app import db
+from app.models.user_model import UserModel
+
+
+class UserLoginHistoryModel(db.Model):
+    __tablename__ = 'user_login_histories'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    ip_address = Column(String(20), nullable=False)
+    agent = Column(String(200))
+    accepted = Column(Boolean, nullable=False, default=False)
+    created_at = Column(DateTime, default=datetime.datetime.now)
+
+    def __init__(self, user_id=None, ip_address=None, agent=None, accepted=None):
+        self.user_id = user_id
+        self.ip_address = ip_address
+        self.agent = agent
+        self.accepted = accepted
--- app/modules/auth/basic.py
+++ app/modules/auth/basic.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import jsonify
+from flask import jsonify, request
 from werkzeug.security import check_password_hash
 
-from app import basic_auth
+from app import db, basic_auth
 from app.models.user_model import UserModel
+from app.models.user_login_history_model import UserLoginHistoryModel
 
 
 @basic_auth.verify_password
@@ -11,10 +12,21 @@ def verify_password(email, password):
     user = UserModel.query\
         .filter(UserModel.email == email)
 
-    if user.count():
-        return check_password_hash(user.first().password, password)
+    accepted = False
 
-    return False
+    if user.count():
+        accepted = check_password_hash(user.first().password, password)
+
+    login_history = UserLoginHistoryModel(
+        user_id=user.first().id,
+        ip_address=request.remote_addr,
+        agent=request.headers.get('User-Agent'),
+        accepted=accepted
+    )
+    db.session.add(login_history)
+    db.session.commit()
+
+    return accepted
 
 
 @basic_auth.error_handler
